cb7bc61e9684a28659fa7b529e234f86d9b9e406
mbu01 <minbu@MINs-MacBook-Pro.local>
2016-09-20 16:04:40 -0400

    adding labelSlider

--- Painting/ViewController.swift
+++ Painting/ViewController.swift
@@ -17,15 +17,25 @@ class ViewController: UIViewController {
     var brushWidth: CGFloat = 10.0
     var opacity: CGFloat = 1.0
     var swiped = false
+    var count_size = 0
+
     
     
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var tempImageView: UIImageView!
     
+    @IBOutlet weak var labelBrush: UILabel!
+    @IBOutlet weak var sliderBrush: UISlider!
+    @IBOutlet weak var titleBrush: UILabel!
+
+    @IBOutlet weak var sizeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderBrush.isHidden = true
+        labelBrush.isHidden = true
+        titleBrush.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,20 +47,41 @@ class ViewController: UIViewController {
     
     
     @IBAction func reset(_ sender: AnyObject) {
+        mainImageView.image = nil
     }
-
-    
-//    @IBAction func share(_ sender: AnyObject) {
-//    }
+  
     
     @IBAction func colorPressed(_ sender: AnyObject) {
     }
-//    @IBAction func pencilPressed(_ sender: AnyObject) {
-//    }
+  
     
     @IBAction func sizePressed(_ sender: AnyObject) {
+        count_size += 1
+        if count_size % 2 == 1 {
+            sliderBrush.isHidden = false
+            labelBrush.isHidden = false
+            titleBrush.isHidden = false
+        } else {
+            sliderBrush.isHidden = true
+            labelBrush.isHidden = true
+            titleBrush.isHidden = true
+        }
+    }
+    
+    
+    @IBAction func eraserPressed(_ sender: AnyObject) {
     }
   
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        if sender == sliderBrush {
+            brushWidth = CGFloat(sender.value)
+            labelBrush.text = NSString(format: "%.2f", brushWidth.native) as String
+        }
+        
+        //drawPreview()
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         swiped = false
         if let touch = touches.first {
@@ -60,25 +91,21 @@ class ViewController: UIViewController {
     }
     
     func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
-        //1
+
         UIGraphicsBeginImageContext(view.frame.size)
         let context = UIGraphicsGetCurrentContext()
         tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
         
-        //2
         context!.move(to: fromPoint)
         context!.addLine(to: toPoint)
         
-        //3
         context!.setLineCap(CGLineCap.round)
         context!.setLineWidth(brushWidth)
         context!.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
         context!.setBlendMode(CGBlendMode.normal)
         
-        //4
         context!.strokePath()
         
-        //5
         tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
         tempImageView.alpha = opacity
         UIGraphicsEndImageContext()
@@ -90,8 +117,6 @@ class ViewController: UIViewController {
         if let touch = touches.first{
             let currentPoint = touch.location(in: view)
             drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
-            
-            //7
             lastPoint = currentPoint
         }
         super.touchesMoved(touches, with: event)
@@ -104,6 +129,11 @@ class ViewController: UIViewController {
         }
         
         // merge tempImageView into mainImageView
+        mergeView()
+        super.touchesEnded(touches, with: event)
+    }
+    
+    func mergeView() {
         UIGraphicsBeginImageContext(mainImageView.frame.size)
         mainImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: CGBlendMode.normal, alpha: 1.0)
         tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: CGBlendMode.normal, alpha: opacity)
@@ -111,7 +141,6 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
         
         tempImageView.image = nil
-        super.touchesEnded(touches, with: event)
     }
 
 
